329b50bf55ba3efd1578b86eca9c5bf248d884c1
bapuAbhijeet <abhijeet15111996@gmail.com>
2020-08-23 16:38:00 +0000

    Spread Oper

--- play.js
+++ play.js
@@ -3,7 +3,20 @@ name : 'Max',
 age  : 29,
 greet: function() {
     console.log("Name is :"+ this.name);
+},
+greet_diff (){
+console.log("Name is :"+ this.name);
 }
 };
 console.log (person);
 person.greet();
+
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
+const hobbies = ['Sports','cooking'] ;
+for(let hobby of hobbies)
+{
+    console.log(hobby);
+}
+console.log(hobbies.map(hobby => 'Hobby  : '+hobby));
